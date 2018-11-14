c841a6312e2076fa12f2c02581254e9756a95280
rohin <rohin0987654321@gmail.com>
2018-11-14 02:38:13 -0800

    init commit

--- movie1/views.py
+++ movie1/views.py
@@ -33,12 +33,12 @@ def vollyball(request):
 
 def listofposts(request):
     post=Posts.objects.all().order_by('published')
-    return render(request , 'movie1/listofposts.html' ,{'post':post})
+    return render(request , 'movie1/listofposts.html' , {"post":post})
 
 def posts_detail(request , slug):
 
     post=get_object_or_404(Posts , slug=slug)
-    return render(request , 'movie1/posts_detail.html' ,{'post':post})
+    return render(request , 'movie1/posts_detail.html' , {'post':post})
 
 
 def facebook(request):
