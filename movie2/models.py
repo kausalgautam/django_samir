5e1203f64df9812b6806c14298b356dc8409fbbc
rohin <rohin0987654321@gmail.com>
2019-01-05 00:56:27 -0800

    init commit

--- movie2/models.py
+++ movie2/models.py
@@ -4,7 +4,7 @@ from django.utils import  timezone
 
 # Create your models here.
 class Posts(models.Model):
-    objects = "movie1"
+    
     STATUS_CHOICES= (
         ('draft','Drafts'),
         ('published','Published'),
--- movie2/views.py
+++ movie2/views.py
@@ -1,12 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from movie2.models import Posts
 
 
 
 
 def listofposts(request):
-    post=Posts.objects.all().order_by('published')
-    arg={'post':post}
-    return render(request , 'movie2/listofposts.html', arg)
+	context = {
+	'post': Posts.objects.all().order_by('published')
+	}
 
+	return render(request, 'movie2/listofposts.html', context)
+
+
+
+def inside(request):
+    message = "Cwelcome"
+
+    return render_to_response('movie2/inside.html', {"message": message})
+
+
+
+
+	
+   
 # Create your views here.
