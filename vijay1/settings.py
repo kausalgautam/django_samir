fe6247fff578702cc0b296fa52acc470ade684a9
rohin <rohin0987654321@gmail.com>
2019-01-20 01:49:24 -0800

    init commit

--- vijay1/settings.py
+++ vijay1/settings.py
@@ -77,17 +77,17 @@ WSGI_APPLICATION = 'vijay1.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'bowl',
+        'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'kausal3gautam###',
         'HOST': 'localhost',
         'PORT': '5432',
     }
 }
+
 
 
 # Password validation
