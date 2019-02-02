cf1a7a67bf944f807ebc9f0f38440b67f3683d2f
learndjango <learndjango3@gmail.com>
2019-02-02 06:31:13 -0800

    created new mail

--- vijay1/settings.py
+++ vijay1/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'vijay1.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'kausal3gautam###',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
